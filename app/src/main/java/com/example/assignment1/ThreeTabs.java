8b7b6bd26628b2289c7d10064a08d2452abb7112
RohmaAr <rohmaarif19@gmail.com>
2024-03-25 22:27:29 +0500

    Screen 1 and 2 done, make dimen.xml

--- app/src/main/java/com/example/assignment1/ThreeTabs.java
+++ app/src/main/java/com/example/assignment1/ThreeTabs.java
@@ -3,11 +3,13 @@ package com.example.assignment1;
 import android.annotation.SuppressLint;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -21,9 +23,10 @@ import androidx.fragment.app.FragmentManager;
 import java.util.Objects;
 
 public class ThreeTabs extends AppCompatActivity {
-    ImageView ivBack;
+    ImageView ivBack,ivFrag;
     FragmentManager fragmentManager;
     LinearLayout llOne,llTwo,llThree;
+    TextView tvFrag;
     Fragment frag_one,frag_two;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -35,11 +38,32 @@ public class ThreeTabs extends AppCompatActivity {
         llTwo=findViewById(R.id.lltwo);
         llThree=findViewById(R.id.llthree);
         fragmentManager=getSupportFragmentManager();
-//        frag_one=Objects.requireNonNull(Objects.requireNonNull(fragmentManager.findFragmentById(R.id.pageone)).requireView();
-//        ivBack.setOnClickListener(v -> {
-//            Intent intent=new Intent(ThreeTabs.this, MainActivity.class);
-//            startActivity(intent);
-//            finish();
-//        });
+        tvFrag = Objects.requireNonNull(fragmentManager.findFragmentById(R.id.pageone)).requireView()
+                .findViewById(R.id.tvFrag);
+        ivFrag=Objects.requireNonNull(fragmentManager.findFragmentById(R.id.pageone)).requireView()
+                .findViewById(R.id.ivFrag);
+        ivBack.setOnClickListener(v -> {
+            Intent intent=new Intent(ThreeTabs.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+        llOne.setOnClickListener(e->{
+            String textSet="PAGE ONE";
+            tvFrag.setText(textSet);
+            ivFrag.setImageResource(R.drawable.hearticon);
+
+        });
+        llTwo.setOnClickListener(e->{
+            String textSet="PAGE TWO";
+            tvFrag.setText(textSet);
+            ivFrag.setImageResource(R.drawable.callicon);
+
+        });
+        llThree.setOnClickListener(e->{
+            Intent intent=new Intent(ThreeTabs.this, InstaProf.class);
+            startActivity(intent);
+            finish();
+
+        });
     }
 }
